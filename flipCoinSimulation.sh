860a93fdae1fa067973a5371f8879838f27ad5e4
shraddha <shraddhat22m@gmail.com>
2020-01-24 11:01:03 +0530

    ADDED:Flipping Coin To Display Head Or Tail

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x 
 echo "Welcome To Flip Coin Simulation"
+
+#flipping coin to print head or tail 
+Flip=$((RANDOM%2))
+if [[ $Flip -eq 0 ]]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
